726fa98665ac4ef50dc87be0c56d5c8e230a2ab7
Bruno Pithon <brunopithondev@gmail.com>
2023-05-27 21:05:29 -0300

    Configurando o banco de dados e o JWT

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get( '/unauthenticated', function () { return ['message' => 'Unauthenticated']; })->name('unauthenticated');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[UserController::class,'register']);
+
+Route::post('login',[UserController::class,'loginUser']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
+
