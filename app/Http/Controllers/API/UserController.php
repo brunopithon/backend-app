0e38ee709acd7fd79aafe618ea06cfe722b202fa
Bruno Pithon <brunopithondev@gmail.com>
2023-05-29 22:14:07 -0300

    Implementando User

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,7 +14,8 @@ use Auth;
 
 class UserController extends Controller
 {
-    public function register(Request $request)
+
+    public function insert(Request $request)
     {
         $validator = Validator::make($request->all(),[
             'name' => 'required|string|max:255',
@@ -40,4 +41,81 @@ class UserController extends Controller
         return response()
             ->json(['data' => $user,'access_token' => $token, 'token_type' => 'Bearer', ]);
     }
+
+
+    public function update(Request $request, User $user)
+    {
+        if (!$request->user()->is_admin AND $request->user()->id != $user->id) {return response()->json(['message' => 'Unauthorized'], 401);}
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'string|max:255|required',
+            'email' => ['string', 'email', 'max:255', Rule::unique('users')->ignore($user), 'required'],
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user->email = $request->email;
+        $user->name = $request->name;
+
+        if ($request->user()->is_admin AND isset($request->is_admin)){
+            $user->is_admin = $request->is_admin;
+        }
+
+        $user->save();
+
+        return response()->json(['User updated successfully.', $user]);
+    }
+
+
+    public function login(Request $request): Response
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'string',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return Response(['message' => $validator->errors()],401);
+        }
+
+        if(Auth::attempt($request->all())){
+
+            $user = Auth::user();
+
+            $success =  $user->createToken('MyApp')->plainTextToken;
+
+            return Response(['token' => $success],200);
+        }
+
+        return Response(['message' => 'email or password wrong'],401);
+    }
+
+
+    public function logout(): Response
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return Response(['data' => 'User Logout successfully.'],200);
+    }
+
+
+
+    public function select(): Response
+    {
+        if (Auth::check()) {
+
+            $user = Auth::user();
+
+            return Response(['data' => $user],200);
+        }
+
+        return Response(['data' => 'Unauthorized'],401);
+    }
+
 }
