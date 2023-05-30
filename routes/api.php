5656c20ef3b3c18eb62ae6ee70a80a43ebaee6e9
Bruno Pithon <brunopithondev@gmail.com>
2023-05-29 22:14:17 -0300

    Criando as rotas de User e Person

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\PersonController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,20 +19,32 @@ use App\Http\Controllers\API\UserController;
 
 Route::get( '/unauthenticated', function () { return ['message' => 'Unauthenticated']; })->name('unauthenticated');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::post('register',[UserController::class,'register']);
+Route::post('insert',[UserController::class,'insert']);
 
 Route::post('login',[UserController::class,'loginUser']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
 
 
+Route::group(['middleware' => 'auth:sanctum'],function(){
 
+    Route::get('person',[PersonController::class,'index']);
+    Route::post('person',[PersonController::class,'store']);
+    Route::get('person/{person}',[PersonController::class,'show']);
+    Route::post('person/{person}',[PersonController::class,'update']);
+    Route::delete('person/{person}',[PersonController::class,'destroy']);
+
+    Route::get('user',[UserController::class,'select']);
+    Route::post('logout',[UserController::class,'logout']);
+
+});
 
 
